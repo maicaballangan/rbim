8b4406d940d67a5d8c9a6e99c86772c0e9b1955e
Maica Ballangan <jamaicaballangan@gmail.com>
2022-04-04 15:00:31 +0800

    Add failedAttempts limit

--- app/controllers/Security.java
+++ app/controllers/Security.java
@@ -1,19 +1,34 @@
 package controllers;
 
 import models.Staff;
+import play.cache.Cache;
 
 /**
  * @author Maica Ballangan
  * @since v1
  */
 public class Security extends Secure.Security {
+    private static final String BUCKET = "security/";
 	
     static boolean authenticate(String username, String password) {
-        Staff staff =  Staff.findById(username.toUpperCase());
-        if (staff != null) {
-            return staff.checkPassword(password);
+        String user = username.toUpperCase();
+        String key = BUCKET + user;
+
+        Integer failedAttemptCnt = (Integer) Cache.get(key);
+        if (failedAttemptCnt != null && failedAttemptCnt > 4) {
+            flash.error("secure.rateLimit");
+            params.flash();
+            render("Secure/login.html");
         }
 
+        Staff staff =  Staff.findById(username);
+        if (staff != null) {
+            if (staff.checkPassword(password)) {
+                return true;
+            }
+        }
+
+        Cache.set(key, failedAttemptCnt != null ? failedAttemptCnt + 1 : 1, "1h");
         return false;
     }
 
